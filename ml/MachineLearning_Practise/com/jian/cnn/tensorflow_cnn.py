76c2d89787585f7a4609e2a31a365e26e5102b94
tianjian <1635374633@qq.com>
2019-04-02 15:48:24 +0800

    神经网络添加注释

--- ml/MachineLearning_Practise/com/jian/cnn/tensorflow_cnn.py
+++ ml/MachineLearning_Practise/com/jian/cnn/tensorflow_cnn.py
@@ -37,34 +37,42 @@ def max_pool_2x2(x):
 
 
 def train(mnist):
-        # 使用占位符
+        # 使用占位符  None代表未知的样本量，784代表像素28*28
         x = tf.placeholder(tf.float32, [None, 784])     # x为特征
         y_ = tf.placeholder(tf.float32, [None, 10])  # y_为label 输出为0-9的one-hot编码 输出层
+
         # 卷积中将1x784转换为28x28x1  [-1,,,]代表样本数量不变 [,,,1]代表通道数
         x_image = tf.reshape(x, [-1, 28, 28, 1])
 
 
         # 第一个卷积层  [5, 5, 1, 32]代表 卷积核尺寸为5x5,1个图像通道,32个不同卷积核，一个层的卷积核的通道数取决于图像通道
+        # 也就是说这一次卷积操作输入是1，输出是32
         # 创建滤波器权值-->加偏置-->卷积-->池化
         W_conv1 = weight_variable([5, 5, 1, 32])
         # 偏置
         b_conv1 = bias_variable([32])
+
+        # 先进行卷积操作，再进行线性修正，relu--- max(0,x)  得到第一卷积层
         h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1) + b_conv1)  # 28x28x1 与32个5x5x1滤波器 --> 28x28x32
 
         print(h_conv1)
+        # 进行池化操作
         h_pool1 = max_pool_2x2(h_conv1) # 28x28x32 -->14x14x32
 
         # 第二层卷积层 卷积核依旧是5x5 通道为32   有64个不同的卷积核，第二层的卷积核通道取决于上一层卷积核的数量，上一层的卷积核数量是32，
         # 那么这一层卷积层就有32层，故本次通道是32
+        # 这次卷积输入是32个，输出是64个
         W_conv2 = weight_variable([5, 5, 32, 64])
         b_conv2 = bias_variable([64])
         h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) #14x14x32 与64个5x5x32滤波器 --> 14x14x64
         h_pool2 = max_pool_2x2(h_conv2) #14x14x64 --> 7x7x64
 
         # h_pool2的大小为7x7x64 转为1-D 然后做FC层
+        # 到了全连接操作，输入为64个7X7的feature矩阵，输出是1024维
         W_fc1 = weight_variable([7 * 7 * 64, 1024])
         b_fc1 = bias_variable([1024])
         h_pool2_flat = tf.reshape(h_pool2, [-1,7 * 7 * 64])  # 7x7x64 --> 1x3136
+        # 这里的操作就不是卷积了，而是正常的线性变换 y = WX + b
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1) #FC层传播 3136 --> 1024
 
         # 使用Dropout层减轻过拟合,通过一个placeholder传入keep_prob比率控制
@@ -73,11 +81,19 @@ def train(mnist):
         h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
         # 将Dropout层的输出连接到一个Softmax层,得到最后的概率输出
+        # 1024进 10出
         W_fc2 = weight_variable([1024, 10])  # MNIST只有10种输出可能
         b_fc2 = bias_variable([10])
-        y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+        # 简单的线性操作之后，通过softmax将最后结果归一化得到当前图像的对10中可能结果的预测概率
+        # softmax就相当于 [-3,2,-1,0]-->[0.0057,0.8390,0.0418,0.1135] 给出一个归一化之后的概率值
+        y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2) # 1024 --> 10
 
         # 定义损失函数,依旧使用交叉熵  同时定义优化器  learning rate = 1e-4
+        # 交叉熵刻画了两个概率分布之间的距离，是分类问题中使用比较广泛的损失函数
+        # 假设有一个三分类问题，某个样例的正确答案是（1，0，0）。某模型经过softmax回归之后的预测答案是(0.5,0.4,0.1)，
+        # 那么这个预测和正确答案之间的交叉熵为：H（（1，0，0），（0.5，0.4，0.1）)=-(1*log0.5+0*log0.4+0*log0.1)≈0.3
+        #                                  H（（1，0，0），（0.8，0.1，0.1）)=-（1*log0.8）≈0.1
+        # 结果约接近正确答案，交叉熵越小，其实看log函数就能看出端倪，正确结果_y与预测结果y_conv的乘积就是 loss = -1xlog(p) p越接近与q loss越接近0
         cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
         train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
@@ -90,7 +106,7 @@ def train(mnist):
             init_op = tf.global_variables_initializer() #初始化所有变量
             sess.run(init_op)
 
-            STEPS = 20000
+            STEPS = 200
             for i in range(STEPS):
                 batch = mnist.train.next_batch(50)
                 if i % 100 == 0:
