035917687d0ea3a6d8f204368ea58aca9e719062
tjsmile <1635374633@qq.com>
2019-04-21 18:38:48 +0800

    gcn 初见

---
+++ ml/MachineLearning_Practise/com/graph/gcn/gcn_karate.py
@@ -0,0 +1,43 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+zkc = nx.karate_club_graph()
+order = sorted(list(zkc.nodes()))
+A = nx.to_numpy_matrix(zkc, nodelist=order)
+I = np.eye(zkc.number_of_nodes())
+A_hat = A + I
+D_hat = np.array(np.sum(A_hat, axis=0))[0]
+D_hat = np.matrix(np.diag(D_hat))
+
+W_1 = np.random.normal(
+    loc=0, scale=1, size=(zkc.number_of_nodes(), 4))
+W_2 = np.random.normal(
+    loc=0, size=(W_1.shape[1], 2))
+
+def relu(x):
+    y = np.maximum(x, 0)
+    return y
+
+def gcn_layer(A_hat, D_hat, X, W):
+    return relu(D_hat**-1 * A_hat * X * W)
+
+def plot_embeddings(embeddings):
+
+    for i,idx in embeddings.items():
+        print(i,idx)
+        plt.scatter(idx[0], idx[1], label='0')
+    plt.legend()
+    plt.show()
+
+H_1 = gcn_layer(A_hat, D_hat, I, W_1)
+H_2 = gcn_layer(A_hat, D_hat, H_1, W_2)
+output = H_2
+
+feature_representations = {
+    node: np.array(output)[node]
+    for node in zkc.nodes()}
+
+print(feature_representations)
+
+plot_embeddings(feature_representations)
--- ml/MachineLearning_Practise/com/graph/gcn/gcn_test.py
+++ ml/MachineLearning_Practise/com/graph/gcn/gcn_test.py
@@ -1,9 +1,20 @@
-
 import numpy as np
-import networkx as nw
+import matplotlib.pyplot as plt
 
-def test():
-    # 一个有向图的邻接矩阵
+def plot_embeddings(embeddings):
+    embeddings = embeddings.tolist()
+    for idx in range(len(embeddings)):
+        print(embeddings[idx])
+        plt.scatter(embeddings[idx][0], embeddings[idx][1], label='0')
+    plt.legend()
+    plt.show()
+
+def relu(x):
+    y = np.maximum(x, 0)
+    return y
+
+if __name__ == '__main__':
+    # 有向图邻接矩阵
     A = np.matrix([
         [0, 1, 0, 0],
         [0, 0, 1, 1],
@@ -12,41 +23,46 @@ def test():
         dtype=float
     )
 
+    # 暂且虚构出每一个节点的特征，有两列
     X = np.matrix([
         [i, -i]
         for i in range(A.shape[0])
     ], dtype=float)
 
-    temp = A * X
+    # 对于0节点只有1是他的邻居。传播之后，0节点的特征是[1,-1],这是0节点邻居特征的和
+    # 每个节点的表征（每一行）现在是其相邻节点特征的和！换句话说，图卷积层将每个节点表示为其相邻节点的聚合
+    C = A * X
 
-    print(temp)
+    # print(C)
 
-def karate_gcn(zkc):
-    order = sorted(list(zkc.nodes()))
-    A = nw.to_numpy_matrix(zkc, nodelist=order)
-    I = np.eye(zkc.number_of_nodes())
+    # 节点的聚合表征不包含它自己的特征！该表征是相邻节点的特征聚合
+    # 为了解决第一个问题，我们可以直接为每个节点添加一个自环 [1, 2]。
+    # 具体而言，这可以通过在应用传播规则之前将邻接矩阵 A 与单位矩阵 I 相加来实现。
+    I = np.matrix(np.eye(A.shape[0]))
+
     A_hat = A + I
-    D_hat = np.array(np.sum(A_hat, axis=0))[0]
-    D_hat = np.matrix(np.diag(D_hat))
+    C1 = A_hat * X
 
-    W_1 = np.random.normal(
-        loc=0, scale=1, size=(zkc.number_of_nodes(), 4))
-    W_2 = np.random.normal(
-        loc=0, size=(W_1.shape[1], 2))
+    # 现在，由于每个节点都是自己的邻居，每个节点在对相邻节点的特征求和过程中也会囊括自己的特征！
+    # print(C1)
 
-    H_1 = gcn_layer(A_hat, D_hat, I, W_1)
-    H_2 = gcn_layer(A_hat, D_hat, H_1, W_2)
-    output = H_2
+    # 计算度矩阵
+    D = np.array(np.sum(A, axis=0))[0]
+    D = np.matrix(np.diag(D))
 
-    feature_representations = {
-        node: np.array(output)[node]
-        for node in zkc.nodes()}
+    # 通过将邻接矩阵 A 与度矩阵 D 的逆相乘，对其进行变换，从而通过节点的度对特征表征进行归一化
+    F = D ** -1 * A * X
+    # print(F)
 
+    plot_embeddings(F)
 
-def gcn_layer(A_hat, D_hat, X, W):
-    return np.relu(D_hat**-1 * A_hat * X * W)
+    W = np.matrix([
+        [1, -1],
+        [-1, 1]
+    ])
 
-if __name__ == '__main__':
-    # test()
-    zkc = nw.karate_club_graph()
-    karate_gcn(zkc)
+    D_hat = A_hat = A + I
+    D_hat ** -1 * A_hat * X * W
+
+    relu(D_hat ** -1 * A_hat * X * W)
+
